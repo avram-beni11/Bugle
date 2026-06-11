9d8a935d0c17e0567b6debcf30dddf959266586e
avram-beni11 <116164939+avram-beni11@users.noreply.github.com>
2026-06-11 15:41:21 +0100

    installed some modules

---
+++ src/components/auth/LogReg.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "/src/style.css";
+
+export default function LogReg() {
+  return (
+    <>
+      <div>
+        <h1>Login Page</h1>
+        <input placeholder="username" />
+      </div>
+    </>
+  );
+}
